17a5cfb5c44bdea4052345e8099e1d5365847f5b
alexanderjquinn@gmail.com <alexanderjquinn@gmail.com@0dc64374-26db-fb16-cfe5-9a2e50596088>
2011-12-13 20:42:31 +0000

    added some missing files, including DataDump.py, which was recently fixed.

---
+++ trunk/DataDump.py
@@ -0,0 +1,125 @@
+# SearchParty - Learning to Search in a Web-Based Classroom
+# Authors: Ben Bederson - www.cs.umd.edu/~bederson
+#          Alex Quinn - www.cs.umd.edu/~aq
+#          University of Maryland, Human-Computer Interaction Lab - www.cs.umd.edu/hcil
+# Date: Originally created July 2011
+# License: Apache License 2.0 - http://www.apache.org/licenses/LICENSE-2.0
+
+from PersonPage import PersonPage
+
+class DataDump(PersonPage):
+	def get(self):
+		import datetime
+		self.load_search_party_context(user_type="teacher")
+
+		if not self.is_teacher:
+			self.redirect_to_teacher_login()
+		else:
+			lesson_code = self.request.get("lesson_code", None)
+			assert lesson_code is not None
+			utc_offset_minutes = int(self.request.get("utc_offset_minutes", 0))
+			utc_offset = datetime.timedelta(minutes=utc_offset_minutes)
+			self._send_tab_delimited_report(lesson_code=lesson_code, utc_offset=utc_offset)
+	
+	def _send_tab_delimited_report(self, lesson_code, utc_offset):
+		import StringIO, csv
+		from model import Student, StudentActivity, Lesson
+		import helpers
+		encoding = "UTF-8"
+
+		lesson = Lesson.get_by_key_name(lesson_code)
+		assert lesson is not None
+
+		if lesson is None or lesson.teacher_key != self.teacher_key:
+			self.write_response_plain_text("ERROR:  Lesson code appears to be incorrect.")
+		else:
+
+			students = Student.fetch_all("lesson =", lesson)
+			task_titles = tuple(task_info[0] for task_info in lesson.tasks)
+			student_key_to_nickname = dict((s.key().name(), s.nickname) for s in students)
+			activities = StudentActivity.fetch_all("lesson =", lesson)
+
+			report_buffer = StringIO.StringIO()
+			excel_writer = UnicodeWriter(report_buffer, "excel-tab", "utf8")
+
+			headers = (
+#						"Lesson_Code",
+					"Timestamp",
+					"Student",
+					"Task_Number",
+					"Task_Name",
+					"Activity_Type",
+					"Query",
+					"Link_URL",
+					"Link_Title",
+					"Is_Helpful",
+					"Answer_Text",
+					"Answer_Explanation"
+			)
+			excel_writer.writerow(headers)
+
+			for activity in activities:
+				student_key = activity.student_key.name()
+				student_nickname = student_key_to_nickname[student_key]
+				timestamp = (activity.timestamp - utc_offset).strftime("%m/%d/%Y %H:%M:%S")
+				task_idx = activity.task_idx
+				task_title = task_titles[task_idx]
+				task_num = task_idx + 1
+				line_parts = (
+#							lesson_code,
+						timestamp,
+						student_nickname,
+						task_num,
+						task_title,
+						activity.activity_type,
+						activity.search,
+						activity.link,
+						activity.link_title,
+						activity.is_helpful,
+						activity.answer_text,
+						activity.answer_explanation
+				)
+#				line_parts = tuple(unicode(p).encode("utf8") for p in line_parts)
+				excel_writer.writerow(line_parts)
+			report_text = report_buffer.getvalue()
+			report_buffer.close()
+
+			content_type = "text/tab-separated-values"
+			filename = "search_party_lesson_%s_activity_as_of_%s.txt"%(lesson_code, helpers.timestamp())
+			self.write_response_as_file(encoded_content=report_text, content_type=content_type, filename=filename, encoding=encoding)
+
+# UnicodeWriter class was taken directly from Python documentation for the csv module.
+# (c) Copyright 1990-2011, Python Software Foundation
+# Licensed under same license as Python itself.
+# Slightly modified by Alex Quinn on 12-12-2011.
+class UnicodeWriter:
+	"""
+	A CSV writer which will write rows to CSV file "f",
+	which is encoded in the given encoding.
+	"""
+	import csv
+
+	def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+		import StringIO, codecs, csv
+		# Redirect output to a queue
+		self.queue = StringIO.StringIO()
+		self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+		self.stream = f
+		self.encoder = codecs.getincrementalencoder(encoding)()
+
+	def writerow(self, row):
+		self.writer.writerow([unicode(s).encode("utf-8") for s in row])
+		# Fetch UTF-8 output from the queue ...
+		data = self.queue.getvalue()
+		data = data.decode("utf-8")
+		# ... and reencode it into the target encoding
+		data = self.encoder.encode(data)
+		# write to the target stream
+		self.stream.write(data)
+		# empty queue
+		self.queue.truncate(0)
+
+	def writerows(self, rows):
+		for row in rows:
+			self.writerow(row)
+
---
+++ trunk/scripts/appcfg.bat
@@ -0,0 +1,2 @@
+@echo %date% %time%
+c:\p\Python25\python25.exe c:\p\AppEngine\appcfg.py --email=alexanderjquinn@gmail.com %1 %2 %3 %4 %5 %6 %7 %8 %9
