9238777acfe299d0f902b9e4751027463824e83b
anne.bobrose@gmail.com <anne.bobrose@gmail.com@0dc64374-26db-fb16-cfe5-9a2e50596088>
2012-06-04 18:57:30 +0000

    Updated copyright

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 # SearchParty - Learning to Search in a Web-Based Classroom
 # Authors: Ben Bederson - www.cs.umd.edu/~bederson
 #          Alex Quinn - www.cs.umd.edu/~aq
+#          Anne Rose - www.cs.umd.edu/hcil/members/~arose
 #          University of Maryland, Human-Computer Interaction Lab - www.cs.umd.edu/hcil
 # Date: Originally created July 2011
 # License: Apache License 2.0 - http://www.apache.org/licenses/LICENSE-2.0
