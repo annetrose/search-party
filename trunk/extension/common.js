a05037482be6beb8d1dbe7db5aad8f5bf4c5a6f9
anne.bobrose@gmail.com <anne.bobrose@gmail.com@0dc64374-26db-fb16-cfe5-9a2e50596088>
2012-07-25 13:04:53 +0000

    Added Chrome extension to repository

---
+++ trunk/extension/common.js
@@ -0,0 +1,103 @@
+//var SEARCH_PARTY_URL = 'http://search-party.appspot.com';
+//var SEARCH_PARTY_URL = 'http://hciltest.appspot.com';
+var SEARCH_PARTY_URL = 'http://localhost:8081';
+
+var DEBUG = true;
+
+var STUDENT_LOGGED_OUT = 0;
+var STUDENT_LOGGED_IN = 1;
+var UNRATED_LINK = 2;
+var HELPFUL_LINK = 3;
+var UNHELPFUL_LINK = 4;
+var TASK_CHANGED = 5;
+
+var EMPTY_SEARCH = '<empty>';
+var RECORDING_TEXT = '>';
+var NOT_RECORDING_TEXT = '||';
+
+var g_login_status = STUDENT_LOGGED_OUT;
+
+function initLocalStorage() {
+	storeTask(0);
+	storeLink('');
+}
+
+function getStoredTask() {
+	return parseInt(localStorage['task_idx']);
+}
+
+function storeTask(task_idx) {
+	localStorage['task_idx'] = task_idx;
+}
+
+function getStoredLink() {
+	return localStorage['link'];
+}
+
+function storeLink(link) {
+	localStorage['link'] = link;
+	localStorage['link_title'] = '';
+}
+
+function getStoredLinkTitle() {
+	return localStorage['link_title'];
+}
+
+function storeLinkTitle(title) {
+	localStorage['link_title'] = title;
+}
+
+function getStoredTab() {
+	return parseInt(localStorage['tab']);
+}
+
+function storeTab(tab) {
+	localStorage['tab'] = tab;
+}
+
+function isStudentLoggedIn() {
+	return g_login_status == STUDENT_LOGGED_IN;
+}
+
+function updateBadge(status) {
+        if (status == STUDENT_LOGGED_OUT) {
+		chrome.browserAction.setBadgeText({text:''});
+		chrome.browserAction.setIcon({path:'images/icon-16-logged-out.png'});
+		g_login_status = status;
+        }
+	else if (status == STUDENT_LOGGED_IN) {
+		chrome.browserAction.setIcon({path:'images/icon-16-logged-in.png'});
+        	chrome.browserAction.setBadgeText({text:""+(getStoredTask()+1)});
+		g_login_status = status;
+        }
+	else if (status == UNRATED_LINK) {
+		chrome.browserAction.setIcon({path:'images/icon-16-unrated.png'});
+	}
+	else if (status == HELPFUL_LINK) {
+		chrome.browserAction.setIcon({path:'images/icon-16-unrated.png'});
+	}
+	else if (status == UNHELPFUL_LINK) {
+		chrome.browserAction.setIcon({path:'images/icon-16-unrated.png'});
+	}
+	else if (status == TASK_CHANGED) {
+        	chrome.browserAction.setBadgeText({text:""+(getStoredTask()+1)});
+	}
+}
+
+function getUrlParameter(url, parameter) {
+	// if hash, only search hash for parameter; otherwise search regular url parameters
+	// Google seems to store "current" values for params in hash sometimes, 
+	// and regular url params contain prior values
+        var search = url;
+	if (!search) search = '';
+        if (search.indexOf('#') != -1) {
+                search = search.substring(search.indexOf('#'));
+        }
+        return decodeURIComponent((new RegExp('[?|&|#]' + parameter + '=' + '([^&;]+?)(&|#|;|$)').exec(search)||[,""])[1].replace(/\+/g, '%20'))||null;
+}
+
+function debug(str) {
+	if (DEBUG) {
+		console.log(str);
+	}
+}
---
+++ trunk/extension/popup.js
@@ -0,0 +1,373 @@
+var g_studentInfo = null;
+
+$(document).ready(function() {
+	loadStudent(true);
+});
+
+function loadStudent(init) {
+	$('#content').hide();
+	$('#loading').show();
+        $.ajax({
+                type: 'POST',
+                url: SEARCH_PARTY_URL+"/student_info",
+                dataType: "json",
+		data: {
+			task_idx: getStoredTask()
+		},
+                cache: false,
+                success: function(data) {
+			g_studentInfo = data;
+			updateBadge(data.status);
+			if (init) {
+				initUI(data);
+			}
+			if (data.status==1) {
+				var taskIndex = getSelectedTaskIndex();
+				var taskDesc = g_studentInfo.lesson.tasks[taskIndex][1];
+				$('#task_desc').html(taskDesc);
+				$('#task_history').html(getHistoryHtml());
+	
+				if (getStoredLink() != '') {
+					var ratingHtml = '<h2>Link Rating</h2>';
+					if (getStoredLinkTitle()!='') ratingHtml += getStoredLinkTitle() + '<br/>';
+					ratingHtml += getStoredLink() + '<br/>';
+					ratingHtml += getRatingSelector()+'<br/>';
+					ratingHtml += '<hr style="color:grey"/>';
+					$('#rating_area').html(ratingHtml);
+					updateLinkRating(getStoredLink());
+					$('input[name=rating]').change(onRatingChanged);
+				}
+				else {
+					$('#rating_area').html('');
+				}
+
+				var responseHtml = '<h2>Response</h2>';
+				responseHtml += getResponseControls() + '<br/>';
+				responseHtml += '<hr style="color:grey"/>';
+				$('#response_area').html(responseHtml);
+				updateResponse();
+				$('#response').change(function() {
+					onUnsavedResponse();
+				});
+				$('#explanation').change(function() {
+					onUnsavedResponse();
+				});
+				$('#submit_response').click(function() {
+					onResponseChanged();
+				});
+			}
+			$('#loading').hide();
+			$('#content').show();
+                },
+		error: function() {
+			g_studentInfo = null;
+			$('#content').html('Error connecting to '+SEARCH_PARTY_URL);
+			$('#loading').hide();
+			$('#content').show();
+		}
+        });
+}
+
+function initUI(data) {
+	var content = '<div style="font-size:14pt; font-weight:bold; float:left">Search Party';
+//	if (data.status==1) {
+//		content += ' <a id="student_link" href="#" class="note">[View web interface]</a>';
+//	}
+	content += '</div>';
+
+	if (data.status == 1) {
+		var nickname = data.anonymous===true ? "Anonymous" : data.student_nickname;
+		content += '<div style="float:right">Welcome '+nickname+' | <a href="#" id="logout_link">Logout</a></div>';
+		content += '<div style="clear:both"></div>';
+		content += '<div class="note" style="float:right">Activity #'+data.lesson.lesson_code+'</div>';
+		content += '<h2>Task</h2>';
+		content += getTaskSelector()+'<br/>';
+		content += '<div id="task_desc"></div>';
+		content += '<hr style="color:grey"/>';
+		content += '<div id="rating_area"></div>';
+		content += '<div id="response_area"></div>';
+		content += '<h2>Search History</h2>';
+		content += '<div id="task_history"></div>';
+	}
+	else {
+		content += '<div style="clear:both"></div>';
+		content += '<p>Please login to <a href="#" id="login_link">Search Party</a> as a student to record searches.</p>';
+	}
+	$('#content').html(content);
+
+	if (data.status==1) {
+		var taskIndex = getStoredTask();
+		$('#task_chooser').val(taskIndex);
+		$('#task_chooser').change(onTaskChanged);
+		$('#student_link').click(onShowWebInterface);
+		$('#logout_link').click(onLogout);
+	}
+	else {
+		$('#login_link').click(onLogin); 
+	}
+}
+
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+	if (request.type=='rating' || request.type=='answer') {
+		g_studentInfo.history.push(request);
+		$('#task_history').html(getHistoryHtml());
+	}
+	else {
+		console.log(request.type+' not handled');
+	}
+
+	if (request.type=='answer') {
+		if (request.timestamp!='') {
+			var timestamp = getFormattedTimestamp(getLocalTime(new Date(request.timestamp)));
+			$('#response_saved').html('Saved '+timestamp);
+		}
+	}
+});
+
+function getTaskSelector() {
+	var html = '<select id="task_chooser">';	
+	for (var i=0; i<g_studentInfo.lesson.tasks.length; i++) {
+		html += '<option value="'+i+'">'+(i+1)+'. '+g_studentInfo.lesson.tasks[i][0]+'</option>';
+	}
+	html += '</select>';
+	return html;
+}
+
+function onTaskChanged() {
+	// SP server not notified about task change so SP UI and 
+	// extension can work on different tasks if they choose
+	storeTask(getSelectedTaskIndex());
+	loadStudent(false);
+	chrome.extension.sendRequest({'type':'task', 'task':getSelectedTaskIndex()});
+}
+
+function getSelectedTaskIndex() {
+	return $('#task_chooser').prop('selectedIndex');
+}
+
+function getRatingSelector() {
+	var html = '<input type="radio" id="helpful" name="rating" value="1"> Helpful</input> ';
+	html += '<input type="radio" id="unhelpful" name="rating" value="0"> Unhelpful</input>';
+	return html;
+}
+
+function onRatingChanged() {
+	var rating = $('input:radio[name=rating]:checked').val();
+	chrome.extension.sendRequest({'type':'rating', 'rating':rating});
+}
+
+function updateLinkRating(url) {
+	var history = g_studentInfo.history;
+	for (var i=history.length-1; i>=0; i--) {
+		var taskItem = history[i];
+                var taskType = taskItem.activity_type;
+		if (taskItem.link == url && taskType == "link_rating") {
+			if (taskItem.is_helpful) {
+				$('#helpful').attr('checked', 'checked');
+			}
+			else {
+				$('#unhelpful').attr('checked', 'checked');
+			}
+			break;
+		}
+	}
+}
+
+function getResponseControls() {
+	var html = '<input type="text" id="response" name="response" value="" style="width:300px"><br/>';
+	html += 'Note<br/>';
+	html += '<textarea rows="2" name="explanation" id="explanation" style="width:300px"></textarea><br/>';
+	html += '<button id="submit_response" name="submit_response">Save</button> ';
+	html += '<span id="response_saved" class="note"></span>';
+	return html;
+}
+
+function getMostRecentResponse() {
+	var response = {'response':'', 'explanation':'', 'timestamp':''};
+	var history = g_studentInfo.history;
+	for (var i=history.length-1; i>=0; i--) {
+		var taskItem = history[i];
+                var taskType = taskItem.activity_type;
+		if (taskType == "answer") {
+			var timestamp = getFormattedTimestamp(getLocalTime(new Date(taskItem.timestamp)));
+			response = {'response':taskItem.answer_text, 'explanation':taskItem.answer_explanation, 'timestamp':timestamp};
+			break;
+		}
+	}
+	return response;
+}
+
+function updateResponse() {
+	var response = getMostRecentResponse();
+	$('#response').val(response.response);
+	$('#explanation').val(response.explanation);
+	if (response.timestamp!='') $('#response_saved').html('Saved '+response.timestamp);
+}
+
+function onUnsavedResponse() {
+	var msg = 'Unsaved changes';
+	var response = $('#response').val();
+	if (response=='') msg += ' - response must not be empty';
+	$('#response_saved').html(msg);
+}
+
+function onResponseChanged() {
+	var response = $('#response').val();
+	if (response != '') {
+		var explanation = $('#explanation').val();
+		chrome.extension.sendRequest({'type':'response', 'response':response, 'explanation':explanation});
+	}
+	else {
+		onUnsavedResponse();
+	}
+}
+
+function onLogin() {
+	updateSearchPartyTab(SEARCH_PARTY_URL+'/student_login?ext=1');
+}
+
+function onShowWebInterface() {
+	var taskIdx = getSelectedTaskIndex();
+	updateSearchPartyTab(SEARCH_PARTY_URL+'/student?task='+(taskIdx+1));
+}
+
+function onLogout() {
+	chrome.extension.sendRequest({'type':'logout'});
+	window.close();
+}
+
+function getHistoryHtml() {
+	var html = '';
+
+	var history = g_studentInfo.history;
+	if (history.length==0) {
+		html = '(none)';
+	}
+
+	//for (var i=0; i<history.length; i++) {
+	for (var i=history.length-1; i>=0; i--) {
+		var taskItem = history[i];
+                var taskType = taskItem.activity_type;
+                if (taskType=='link_rating') {
+                        if (taskItem.is_helpful) taskType='link_helpful';
+                        else taskType='link_unhelpful';
+                }
+
+		var timestamp = getFormattedTimestamp(getLocalTime(new Date(taskItem.timestamp)));
+                var type = '';
+                var details = '';
+                if (taskType=='search') {
+                        type = 'Query';
+			var searchType = getGoogleSearchType(taskItem.link);
+			if (searchType != 'normal') {
+				type += '<br/><span class="note">('+searchType+')</span>';
+			}
+                        details = taskItem.search;
+               	        if (taskItem.link) details = '<a href="'+taskItem.link+'" target="_blank">'+taskItem.search+'</a>';
+                }
+                else if (taskType=='link') {
+                        type = "Visited Link";
+			if (taskItem.search == EMPTY_SEARCH) {
+				type += '<br/><span class="note">(direct)</span>';
+			}
+			details = '';
+			if (taskItem.link_title!=taskItem.link) {
+                        	details = taskItem.link_title+'<br/>';
+			}
+               	        details += '<a href="'+taskItem.link+'" target="_blank">'+taskItem.link+'</a>';
+                }
+                else if (taskType=='link_helpful') {
+		    // page title not stored in rating action
+                    type = "Rated Helpful";
+                    details = '';
+		    if (taskItem.link_title!=null && taskItem.link_title!=taskItem.link) {
+                       	details = taskItem.link_title+'<br/>';
+		    }
+                    details += '<a href="'+taskItem.link+'" target="_blank">'+taskItem.link+'</a>';
+                }
+                else if (taskType=='link_unhelpful') {
+		    // page title not stored in rating action
+                    type = "Rated Unhelpful";
+                    details = '';
+		    if (taskItem.link_title!=null && taskItem.link_title!=taskItem.link) {
+                       	details = taskItem.link_title+'<br/>';
+		    }
+                    details += '<a href="'+taskItem.link+'" target="_blank">'+taskItem.link+'</a>';
+                }
+                else if (taskType=='answer') {
+                    type = "Response";
+                    details = taskItem.answer_text;
+                    if (taskItem.answer_explanation) details += '<br/><em>'+taskItem.answer_explanation+'</em>';
+                }
+
+		html += '<p class="historyitem">';
+		html += '<span class="itemtype">'+type+'</span>';
+		html += '<span class="itemdetail">'+details+'</span>';
+		html += '<span class="itemtime">'+timestamp+'</span>';
+		html += '</p>';
+        }
+	return html;
+}
+
+function getGoogleSearchType(url) {
+	var type = 'normal';
+	if (url) {
+		var param = getUrlParameter(url, 'tbm')
+		if (param == 'isch') {
+			type = 'image';
+		}
+		else if (param == 'vid') {
+			type = 'video';
+		}
+		else if (param == 'nws') {
+			type = 'news';
+		}
+		else if (param == 'shop') {
+			type = 'shopping';
+		}
+		else if (param == 'plcs') {
+			type = 'places';
+		}
+	}
+	return type;
+}
+
+function updateSearchPartyTab(url) {
+	chrome.tabs.query({}, function(tabs) {
+		var spTabId = -1;
+		for (i in tabs) {
+			var tab = tabs[i];
+			if (tab.url.indexOf(SEARCH_PARTY_URL) != -1) {
+				spTabId = tab.id;
+				break;
+			}
+		}
+
+		if (spTabId != -1) {
+			chrome.tabs.update(spTabId, {'url':url, 'active':true}, null);
+		}
+		else {
+			chrome.tabs.create({'url':url}, null);
+		}
+	});
+}
+
+function getLocalTime(gmt)  {
+    var min = gmt.getTime() / 1000 / 60; // convert gmt date to minutes
+    var localNow = new Date().getTimezoneOffset(); // get the timezone offset in minutes
+    var localTime = min - localNow; // get the local time
+    return new Date(localTime * 1000 * 60); // convert it into a date
+}
+
+function getFormattedTimestamp(ts) {
+    var month = ''+(ts.getMonth()+1);
+    if (month.length==1) month = '0' + month;
+    var day = ''+ts.getDate();
+    if (day.length == 1) day = '0' + day;
+    var date =  month + '/' + day + '/'+ (ts.getFullYear()+'').substr(2);
+    var hours = ''+ts.getHours();
+    var mins = ''+ts.getMinutes();
+    if (mins.length == 1) mins = '0' + mins;
+    var time = hours + ':' + mins;
+    return date + '&nbsp;' + time;
+}
